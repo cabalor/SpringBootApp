54567b0f05e0f743f918bb427eabc9c75a5787a1
cabalor <cabalor@gmail.com>
2017-10-04 17:00:32 +0200

    working

--- src/main/java/pl/cbl/controller/MainController.java
+++ src/main/java/pl/cbl/controller/MainController.java
@@ -1,5 +1,18 @@
 package pl.cbl.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class MainController {
 
+	@GetMapping("/")
+	public String main(Model m) {
+		m.addAttribute("welcome", "first thymeleaf site");
+		
+		
+		return "index";
+	}
+	
 }
