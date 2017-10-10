994f6addc2399bb0659e99cf0bc66307be04fa17
cabalor <cabalor@gmail.com>
2017-10-10 20:44:28 +0200

    th

--- src/main/java/pl/cbl/controller/AlbumController.java
+++ src/main/java/pl/cbl/controller/AlbumController.java
@@ -1,8 +1,12 @@
 package pl.cbl.controller;
 
+import java.util.Arrays;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import pl.cbl.data.MusicAlbum;
 
@@ -18,6 +22,17 @@ public class AlbumController {
 	}
 	
 	
+	@PostMapping("/addAlbum")
+	public String added(@ModelAttribute MusicAlbum album, Model mod) {
+		mod.addAttribute("msg", "Album "+album + " was added to db");
+		return "start";
+	}
+	
+	@GetMapping("/albums")
+	public String albums(Model m) {
+		m.addAttribute("albums", Arrays.asList(new MusicAlbum()));
+		return "albums";
+	}
 	
 	
 }
