d796533ae7fd49758f4627e3bf1f1af86ee1514f
cabalor <cabalor@gmail.com>
2017-10-15 18:04:51 +0200

    album cont

--- src/main/java/pl/cbl/controller/AlbumController.java
+++ src/main/java/pl/cbl/controller/AlbumController.java
@@ -1,8 +1,20 @@
 package pl.cbl.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import pl.cbl.data.MusicAlbum;
 
 @Controller
 public class AlbumController {
 
+	
+	@GetMapping
+    public String home(Model model) {
+        model.addAttribute("timeData", new MusicAlbum();
+        return "album";
+    }
+	
+	
 }
--- src/main/java/pl/cbl/repository/AlbumRepo.java
+++ src/main/java/pl/cbl/repository/AlbumRepo.java
@@ -1,9 +1,11 @@
 package pl.cbl.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import pl.cbl.data.MusicAlbum;
 
+@Repository
 public interface AlbumRepo extends JpaRepository<MusicAlbum, Long>{
 
 }
